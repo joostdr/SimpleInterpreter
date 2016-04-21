660d17bfbb893067c8a3666ba1a67ed81f9513ea
Joost de Ruijter <joost1235@hotmail.com>
2016-04-22 00:52:27 +0200

    added recursive function for while, commented out some stuff

--- SimpleInterpreter/SimpleInterpreter/Program.fs
+++ SimpleInterpreter/SimpleInterpreter/Program.fs
@@ -1,8 +1,10 @@
 ﻿open System
 
+type Do = Do
 (*let (<<) int1 int2 = isSmaller(int1,int2)
-let (>>) int1 int2 = isBigger(int1, int2)
-let (:=) int1 int2 = isEquals(int1, int2)*)
+let (>>) int1 int2 = isBigger(int1, int2)                       //some extra possible ways to compare integers
+let (:=) int1 int2 = isEquals(int1, int2
+*)
 
 type Expr =
     | Integer of int
@@ -14,10 +16,14 @@ type Instruction =
     | Add of Expr * Expr
     | Assign of string * Expr
     | Sequence of Instruction * Instruction
-//    | While of Expr * Instruction
+    | While of Expr * Instruction
     | For of Expr * Expr * Expr * Instruction
 
-
+let rec eval (e:Expr) : Expr =
+  match e with
+  | Integer i -> Integer i
+  | Boolean b -> Boolean b
+  | String x -> String x
 
 let checkInt (e:Expr) : bool =
     match e with 
@@ -29,11 +35,17 @@ let parseInt (e:Expr) : int =
     match e with
     | Integer i -> i
 
+(*let rec _while (e:Expr) (instr:Instruction) =
+            match eval e with
+                | Boolean true -> 
+                 _while e instr                                                 //run should still be called
+                | _ -> printfn "Break"*)
+
 let rec run (i:Instruction) =
     match i with
     | PrintLine(x) ->
       printfn "%A" x
-    | Add(int1,int2) -> 
+    | Add(int1,int2) ->                                 //add takes two integers and sums them, throws error if types are not 2 ints
       if checkInt int1 && checkInt int2 then 
          let in1 = parseInt(int1) 
          let in2 = parseInt(int2)
@@ -43,7 +55,7 @@ let rec run (i:Instruction) =
     | Sequence(s1,s2) ->
       run s1
       run s2
-    | For(expr, expr2, expr3, instr) -> 
+   (* | For(expr, expr2, expr3, instr) ->                                  //for not complete yet
       if checkInt expr && checkInt expr2 && checkInt expr3 then
          let intStart = parseInt(expr)
          let intLimit = parseInt(expr2)
@@ -51,25 +63,25 @@ let rec run (i:Instruction) =
          
          if intStart < intLimit then
             let result = intStart + intIncrement
-            printfn "%A" result
+            printfn "%A" result *)
+     (* | While (expr,instr) ->                                           //while is recursive but doesn't run an instruction
+        _while expr instr*)
 
-
-    (*| While(bool,instr) -> 
-      if !bool then
-        run instr
-      else if bool then
-        run instr *)
         
+let __while expr (_:Do) instr = While(expr,instr)
+let _do = Do
 let (++) int1 int2 = Add(int1,int2)       
 let (><) s1 s2 = Sequence(s1,s2)
-//let _for expr1 expr2 expr3 instr = For(expr1, expr2, expr3 instr)
-// let _while expr instr = While(expr,instr)
 let program = PrintLine(String "Foo") >< 
               PrintLine(String "Bar") ><
               PrintLine(Integer 2) ><
-              Integer 5 ++ Integer 8
+              Integer 5 ++ String "p" ><
+              (__while (Boolean true) _do
+                    (PrintLine(String "String")))
 
 
 run program
 let unused = Console.ReadLine()
+
+
 
